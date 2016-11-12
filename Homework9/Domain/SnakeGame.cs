31fe58afbb74f22fb1d36705f319ed371f11034a
jponc021 <jponc021@PG6-106-R1S8>
2016-11-12 10:36:27 -0500

    Started to work on the snake and other domain classes.

---
+++ Homework9/Domain/SnakeGame.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Homework9.Domain
+{
+    public class SnakeGame : IDisposable
+    {
+        private Snake Snake;
+        private Timer MoveTimer;
+        private Timer AppleTimer;
+
+        public SnakeGame()
+        {
+            Snake = new Snake(new Point(0,0), Direction.Right);
+
+            MoveTimer = new Timer();
+            MoveTimer.Elapsed += MoveNow;
+            MoveTimer.Interval = 500;
+            MoveTimer.Enabled = true;
+
+            AppleTimer = new Timer();
+            AppleTimer = 
+        }
+
+        private void MoveTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            
+        }
+
+        public void EndGame()
+        {
+            MoveTimer.Enabled = false;
+        }
+
+
+        public void Dispose()
+        {
+            MoveTimer?.Dispose();
+        }
+
+        
+    }
+}
